3cf7804983d940735f94df2764288b2de01e3371
🐥 Dimitri Reifschneider <dimitri.reifschneider@deutschebahn.com>
2024-01-25 16:41:55 +0100

    Initial commit

---
+++ test/carpe-diem.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CarpeDiem from '../lib/carpe-diem-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/carpe-diem-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CarpeDiem.CarpeDiemStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
